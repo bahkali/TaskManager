cdc6a105a1b5bc81c70415ab9d93a8e44b8f0beb
kaly <kalimamadou@gmail.com>
2022-03-03 13:35:53 -0800

    Added the not found route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./src/route/tasks");
+const notFound = require("./src/middleware/not-found");
 const connectDB = require("./src/db/connect");
 
 require("dotenv").config();
@@ -14,11 +15,10 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello kaly");
-});
-
 app.use("/api/v1/tasks", routes);
+
+//404 routes
+app.use(notFound);
 
 // set port and connect to db
 const PORT = 3500 || process.env.PORT;
